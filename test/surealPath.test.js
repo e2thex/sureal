9971470f691843d51f1485634fa60988e32676db
e2thex <erik@e2thex.org>
2014-07-15 08:37:10 -0500

    update surealPath with better test names

--- test/surealPath.test.js
+++ test/surealPath.test.js
@@ -1,32 +1,30 @@
-describe("surealPath() Module", function() {
+describe("@module surealPath", function() {
   path = surealPath();
-  describe("Base", function() {
+  describe("@function surealPath it @is used to create a SurealPath from a path describe in a string", function() {
     it('should be a function', function() {
       path.should.be.type('function');
     });
-    it("should return an object", function() {
+    it("should @return a @type SurealPath", function() {
       var obj = path();
       obj.should.be.an.Object;
     });
-    describe("path Parameter", function() {
-      it("should only accept strings", function() {
-        (function() {
-          var path = path({});
-        }).should.throw();
-        (function() {
-          var path = path([]);
-        }).should.throw();
-      });
+    it("should accept a @param path of @type String", function() {
+      (function() {
+        var path = path({});
+      }).should.throw();
+      (function() {
+        var path = path([]);
+      }).should.throw();
     });
-    describe("Object", function() {
+    describe("The @class SurealPath @is an object that describes a path to a sureal endpoint ", function() {
       var pathObj = path();
-      it(".predicate should exist as a property", function() {
+      it("should have @property predicate", function() {
         pathObj.should.have.property("predicate");
       });
-      it(".subject should exist as a property", function() {
+      it("should have @property subject", function() {
         pathObj.should.have.property("subject");
       });
-      it(".identifier should exist as a property", function() {
+      it("should have @property identifier", function() {
         pathObj.should.have.property("identifier");
       });
       describe("default value", function() {
@@ -55,33 +53,38 @@ describe("surealPath() Module", function() {
       });
     });
   });
-  describe("path.validate", function() {
+  describe("The @function surealPath.validate @is used to validate a object as a surealPath", function() {
     it("should be a function", function () {
       path.validate.should.be.type('function');
     });
-    it("should not throw if passed a valid path Object", function() {
+    it("should no @throw @type error ifa pathObject is the @param candidate of @type Anything which @is the item to check", function() {
       (function() {
         var pathObj = path();
         path.validate(pathObj);
       }).should.not.throw();
     });
-    it("should throw an error if subject is missing", function () {
+    it("should @throw error if subject is missing", function () {
       (function() {
         var pathObj = {}
         path.validate(pathObj);
       }).should.throw();
     });
-    it("should throw an error if predicate is missing", function () {
+    it("should @throw an error if predicate is missing", function () {
       (function() {
         var pathObj = {subject: ""}
         path.validate(pathObj);
       }).should.throw();
     });
-    it("should throw an error if identifier is missing", function () {
+    it("should @throw an error if identifier is missing", function () {
       (function() {
         var pathObj = {subject: "", predicate: ""}
         path.validate(pathObj);
       }).should.throw();
+    });
+    it("should @return @type SurealPath that @is the object pass in as the candidate", function() {
+        var pathObj = path();
+        var retPathObj = path.validate(pathObj);
+        retPathObj.should.equal(pathObj);
     });
   });
 });
